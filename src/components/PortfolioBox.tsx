5aa1ca3fa9ba51848908cd5eb364743bf0f97c31
JeffersonMontoya <112036905+JeffersonMontoya@users.noreply.github.com>
2025-01-14 13:43:51 -0500

    cambios

--- src/components/PortfolioBox.tsx
+++ src/components/PortfolioBox.tsx
@@ -1,51 +1,72 @@
+"use client"
+
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "framer-motion";
+
+interface PortfolioItem {
+    id: number;
+    title: string;
+    image: string;
+    urlGithub: string;
+    technologies: string[]; 
+}
 
 interface PortfolioBoxProps {
-    data: {
-        id: number,
-        title: string
-        image: string
-        urlGithub: string
-        urlDemo: string
-    }
+    data: PortfolioItem;
 }
 
-const PortfolioBox = (props: PortfolioBoxProps) => {
-    const { data } = props
-    const { id, title, image, urlDemo, urlGithub } = data
+const PortfolioBox = ({ data }: PortfolioBoxProps) => {
+    const { id, title, image, urlGithub, technologies } = data;
 
     return (
-        <div
+        <motion.div
             key={id}
-            className="p-4 border border-teal-50 rounded-xl"
+            className="p-4 border border-teal-50 rounded-xl shadow-lg hover:shadow-xl hover:scale-105 transition-all duration-300 transform hover:translate-y-[-4px]"
+            initial={{ opacity: 0, x: -100 }} 
+            animate={{ opacity: 1, x: 0 }} 
+            exit={{ opacity: 0, x: 100 }} 
+            transition={{ type: "spring", stiffness: 300, damping: 30 }} 
         >
-            <h3 className="mb-4 text-xl">{title}</h3>
-            <Image
-                src={image}
-                alt="Image"
-                width={200} height={200} className="w-full md:w-[200px] rounded-2xl h-auto"
-            />
+            <h3 className="mb-4 text-xl font-bold text-center">{title}</h3>
+            
+            <div className="relative w-full aspect-[4/3] overflow-hidden">
+                <Image
+                    src={image}
+                    alt={title}
+                    width={400}
+                    height={300}
+                    className="w-full h-full object-cover object-top"
+                />
+            </div>
+            <div className="flex flex-wrap gap-2 mt-4 mb-6 justify-center">
+                {technologies.map((tech, index) => (
+                    <span
+                        key={index}
+                        className="px-4 py-1 text-sm font-medium bg-gray-800 text-white rounded-full shadow-md hover:bg-gray-900"
+                    >
+                        {tech}
+                    </span>
+                ))}
+            </div>
 
-            <div className="flex gap-5 mt-5">
-                <Link
-                    href={urlGithub}
-                    target="_blank"
-                    className="p-2 transition duration-150 rounded-lg bg-slate-500 hover:bg-slate-500/80"
-                >
-                    Github
-                </Link>
-
-                <Link
-                    href={urlDemo}
-                    target="_blank"
-                    className="p-2 transition duration-150 rounded-lg bg-secondary hover:bg-secondary/80"
-                >
-                    Live demo
+            <div className="flex justify-between gap-2 m-2">
+                <Link href={urlGithub} target="_blank" rel="noopener noreferrer">
+                    <motion.button
+                        className="px-6 py-3 bg-teal-600 text-white rounded-lg text-sm font-semibold shadow-md hover:shadow-lg hover:bg-teal-700 transition-all duration-300"
+                        whileHover={{
+                            scale: 1.1,  
+                            rotate: 5,  
+                            transition: { type: "spring", stiffness: 400, damping: 10 } 
+                        }}
+                        whileTap={{ scale: 0.95 }} 
+                    >
+                        Ver en GitHub
+                    </motion.button>
                 </Link>
             </div>
-        </div>
+        </motion.div>
     );
-}
+};
 
-export default PortfolioBox
+export default PortfolioBox;
--- src/data/data.tsx
+++ src/data/data.tsx
@@ -1,57 +1,76 @@
-import { Github, Facebook, Mail,  HomeIcon, UserRound, CodeSquare, BookText   , Crop  , Pencil , Computer , Rocket , Book , Code , Database , Youtube , Linkedin ,  Twitter , Rss , Twitch } from "lucide-react"; 
+import {
+  Github,
+  Facebook,
+  Mail,
+  HomeIcon,
+  UserRound,
+  CodeSquare,
+  BookText,
+  Crop,
+  Pencil,
+  Computer,
+  Rocket,
+  Book,
+  Code,
+  Database,
+  Youtube,
+  Linkedin,
+  Twitter,
+  Rss,
+  Twitch,
+  Instagram,
+} from "lucide-react";
+import { FaWhatsapp } from "react-icons/fa";
 
 export const itemsNavbar = [
   {
     id: 1,
-    title: 'Home',
-    icon: <HomeIcon size={25} color='#fff' strokeWidth={1} />,
-    link: '/'
+    title: "Home",
+    icon: <HomeIcon size={25} color="#fff" strokeWidth={1} />,
+    link: "/",
   },
   {
     id: 2,
-    title: 'User',
-    icon: <UserRound size={25} color='#fff' strokeWidth={1} />,
-    link: '/about'
+    title: "User",
+    icon: <UserRound size={25} color="#fff" strokeWidth={1} />,
+    link: "/about",
   },
   {
     id: 3,
-    title: 'Services', 
-    icon: <BookText  size={25} color='#fff' />, 
-    link: '/services'
+    title: "Services",
+    icon: <BookText size={25} color="#fff" />,
+    link: "/services",
   },
   {
     id: 4,
-    title: 'Project',
-    icon: <CodeSquare  size={25} color='#fff' strokeWidth={1} />,
-    link: '/portfolio'
-  }
+    title: "Project",
+    icon: <CodeSquare size={25} color="#fff" strokeWidth={1} />,
+    link: "/portfolio",
+  },
 ];
 
 export const socialNetworks = [
   {
-      id: 1,
-      logo: <Youtube size={30} strokeWidth={1} />,
-      src: "#!",
+    id: 1,
+    logo: <Linkedin size={30} strokeWidth={1} />,
+    src: "#!",
   },
   {
-      id: 2,
-      logo: <Linkedin size={30} strokeWidth={1} />,
-      src: "#!",
+    id: 2,
+    logo: <FaWhatsapp size={30} strokeWidth={1} />,
+    src: "#!",
   },
+
   {
-      id: 3,
-      logo: <Twitter size={30} strokeWidth={1} />,
-      src: "#!",
+    id: 3,
+    logo: <Instagram size={30} strokeWidth={1} />,
+    src: "#!",
   },
+
   {
-      id: 4,
-      logo: <Rss size={30} strokeWidth={1} />,
-      src: "#!",
-  },
-  {
-      id: 5,
-      logo: <Twitch size={30} strokeWidth={1} />,
-      src: "#!",
+    id: 4,
+    logo: <Mail size={30} strokeWidth={1} />,
+    src: "#!",
   },
 ];
 
@@ -79,13 +98,12 @@ export const dataCounter = [
   },
 ];
 
-
 export const dataAboutPage = [
   {
     id: 1,
     title: "Desarrollador Full Stack",
     subtitle: "Prácticas SENA - AndesBPO",
-    description: 
+    description:
       "Realicé mis prácticas profesionales en AndesBPO, donde participé en varios proyectos enfocados en la optimización de procesos empresariales. Diseñé y desarrollé sistemas CRM, integrando funcionalidades tanto en el frontend como en el backend, utilizando tecnologías modernas para satisfacer las necesidades del cliente.",
     date: "Nov 2023",
   },
@@ -93,7 +111,7 @@ export const dataAboutPage = [
     id: 2,
     title: "Estudiante de Desarrollo Web",
     subtitle: "Coderhouse",
-    description: 
+    description:
       "Completé cursos en Desarrollo Web y JavaScript en Coderhouse, donde adquirí habilidades sólidas en programación, diseño responsivo y buenas prácticas en desarrollo web. Implementé proyectos prácticos que consolidaron mi comprensión del ecosistema web moderno.",
     date: "May 2022",
   },
@@ -101,23 +119,24 @@ export const dataAboutPage = [
     id: 3,
     title: "Desarrollador Frontend y Backend",
     subtitle: "Proyectos Personales - CRM",
-    description: 
+    description:
       "Desarrollé proyectos CRM completos, integrando soluciones frontend atractivas y funcionales con un backend robusto. Este trabajo me permitió perfeccionar mis habilidades en tecnologías como React, Django, y Laravel, así como en la gestión de bases de datos y despliegue de aplicaciones.",
     date: "Ago 2021",
   },
- 
 ];
 
 export const serviceData = [
   {
     icon: <Pencil />,
     title: "Diseño web",
-    description: "Diseño creativo y profesional de interfaces web intuitivas y atractivas, centradas en la experiencia del usuario",
+    description:
+      "Diseño creativo y profesional de interfaces web intuitivas y atractivas, centradas en la experiencia del usuario",
   },
   {
     icon: <Computer />,
     title: "Desarrollo web",
-    description: "Diseño y desarrollo de sitios web a medida, adaptados a tus necesidades",
+    description:
+      "Diseño y desarrollo de sitios web a medida, adaptados a tus necesidades",
   },
   {
     icon: <Code />,
@@ -131,40 +150,26 @@ export const serviceData = [
   },
 ];
 
-
-
-
-
 export const dataPortfolio = [
   {
-      id: 1,
-      title: "CRM",
-      image: "/image-1.jpg",
-      urlGithub: "#!",
-      urlDemo: "#!",
+    id: 1,
+    title: "CRM",
+    image: "/crm.jpeg",
+    urlGithub: "https://github.com/Alexsanta045/AndesCobra-Front",
+    technologies: ["Next.js", "Django", "PostgreSQL"], // Tecnologías utilizadas
   },
   {
-      id: 2,
-      title: "Cesde",
-      image: "/image-2.jpg",
-      urlGithub: "#!",
-      urlDemo: "#!",
+    id: 2,
+    title: "Cesde",
+    image: "/cesde.jpeg",
+    urlGithub: "https://github.com/JeffersonMontoya/cesde",
+    technologies: ["Django", "Python", "PostgreSQL"], // Tecnologías utilizadas
   },
   {
-      id: 3,
-      title: "Agenda Pro",
-      image: "/image-3.jpg",
-      urlGithub: "#!",
-      urlDemo: "#!",
+    id: 3,
+    title: "Portafolio",
+    image: "/portafolio.jpeg",
+    urlGithub: "https://github.com/JeffersonMontoya/portafolio",
+    technologies: ["Next.js", "React", "Node.js"], // Tecnologías utilizadas
   },
-  {
-      id: 4,
-      title: "Portafolio",
-      image: "/image-4.jpg",
-      urlGithub: "#!",
-      urlDemo: "#!",
-  },
- 
 ];
-
-
