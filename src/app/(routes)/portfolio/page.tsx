d3bf4298232899f905b46c120fe94140bbfd4a84
JeffersonMontoya <112036905+JeffersonMontoya@users.noreply.github.com>
2026-01-22 12:08:24 -0500

    actualizacion projects

--- src/app/(routes)/portfolio/page.tsx
+++ src/app/(routes)/portfolio/page.tsx
@@ -11,7 +11,6 @@ const PortfolioPage = () => {
   return (
     <ContainerPage>
       <TransitionPage />
-      <AvatarPortfolio />
       <CircleImage />
       <div className="flex flex-col justify-start px-4 -mt-14">
         <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-2">
@@ -19,10 +18,12 @@ const PortfolioPage = () => {
           <span className="font-bold text-secondary">trabajos realizados</span>
         </h1>
 
-        <div className="relative z-10 grid gap-6 mx-auto mt-4 max-w-full sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 lg:ml-36 xl:ml-52">
-          {dataPortfolio.map((data) => (
-            <PortfolioBox key={data.id} data={data} />
-          ))} 
+        <div className="relative z-10 px-4 py-10 mx-auto max-w-7xl">
+          <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center">
+            {dataPortfolio.map((data) => (
+              <PortfolioBox key={data.id} data={data} />
+            ))}
+          </div>
         </div>
       </div>
     </ContainerPage>
